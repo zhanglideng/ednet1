1fbe3370a11f305f589da58531af96300f408866
random <80976700@qq.com>
2019-12-11 20:00:45 +0800

    添加了对MS-SSIM的GPU运算，加速了训练过程。

--- utils/loss.py
+++ utils/loss.py
@@ -8,6 +8,6 @@ def l2_loss(input_image, output_image):
 
 
 def ssim_loss(input_image, output_image):
-    losser = MS_SSIM(max_val=1)
+    losser = MS_SSIM(max_val=1).cuda()
     # losser = MS_SSIM(data_range=1.).cuda()
     return 1 - losser(input_image, output_image)
