253a74a45c492b20dfe04b06cadb4b7f70540856
random <80976700@qq.com>
2020-01-10 11:04:17 +0800

    修改了无雾图的t值

--- utils/loss.py
+++ utils/loss.py
@@ -4,10 +4,10 @@ from utils.ms_ssim import *
 
 def l2_loss(input_image, output_image):
     l2_loss_fn = torch.nn.MSELoss(reduction='mean').cuda()
-    return l2_loss_fn(input_image, output_image)
+    return l2_loss_fn(input_image, output_image)*100
 
 
 def ssim_loss(input_image, output_image):
     losser = MS_SSIM(max_val=1).cuda()
     # losser = MS_SSIM(data_range=1.).cuda()
-    return 1 - losser(input_image, output_image)
+    return 1 - losser(input_image, output_image)*100
